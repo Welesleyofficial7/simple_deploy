654497f872cb664caa594251de4ed7156b3fbcbb
app4d <app4d-official@yandex.ru>
2024-12-10 13:08:14 +0300

    first

--- src/main/java/org/example/ExampleResource.java
+++ src/main/java/org/example/ExampleResource.java
@@ -11,7 +11,7 @@ public class ExampleResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkuss REST";
+        return "Hello from Quarkus REST";
     }
 }
 
